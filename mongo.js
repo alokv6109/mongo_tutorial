f8901e9717bdf8d15ce20a40c557b5c8dc4d8e8a
v-alok <v.alok@samsung.com>
2022-06-13 01:32:11 +0530

    the first tutorial is added

---
+++ mongo.js
@@ -0,0 +1,37 @@
+{
+  name:'complete java',
+  pages:500,
+  prices:1000
+
+
+}
+
+/*
+INSERT
+insertOne
+
+insertMany
+
+updated
+
+update
+updateOne
+updateMany
+update operator
+
+Delete
+
+remove()
+deleteOne()
+deleetMany()
+
+select/finbd
+limit
+sort
+find()
+pretty()
+query operator
+
+
+
+*/
